c20e66223c6249bfff351c158f1689b2458e5784
geekyUtkarsh99 <utkarshchoudhary80@gmail.com>
2022-02-20 19:44:43 +0530

    mysql init

--- edugateApi/app.py
+++ edugateApi/app.py
@@ -1,9 +1,38 @@
-from flask import Flask,render_template
+from flask import Flask, request, abort, jsonify, make_response
+import database.mysqlcore as mysqlcore
 
-app = Flask(__name__,static_folder='../build',static_url_path='/')
+app = Flask(__name__, static_folder='../build', static_url_path='/')
+mysql = mysqlcore.dbHandler()
 
 
 @app.route('/')
 def init():  # put application's code here
     return app.send_static_file('index.html')
 
+
+@app.route('/addbanner', methods=['GET'])
+def add_banner():
+    args = request.args
+    banner = args['banner']
+    try:
+        if banner is not None:
+            response = mysql.store_banner(banner)
+            if response:
+                return make_response(jsonify({'message': 'banner added'})), 201
+            else:
+                return make_response(jsonify({'message': 'something went wrong'})), 503
+    except Exception as e:
+        abort(503, e)
+
+
+@app.route('/getbanner', methods=['GET'])
+def get_banner():
+    try:
+        response = mysql.get_banners()
+        if response is not None:
+            return make_response(jsonify({'status': 200, 'banners': response})), 200
+        else:
+            return make_response(jsonify({'status': 404, 'banners': []})), 404
+
+    except Exception as e:
+        abort(404, e)
---
+++ edugateApi/database/mysqlcore.py
@@ -0,0 +1,70 @@
+import pymysql
+import random
+import string
+
+# import flaskext.mysql as mysql
+# import mysql.connector as connector
+password = 'edugate@digitalinclined'
+
+
+class dbHandler:
+    def __init__(self):
+        self.connection = pymysql.connect(user="root", password=password, database="edugatedb", host="localhost")
+
+    @staticmethod
+    def generate_banner_id():
+        len = 8
+        return "".join(random.choices(string.ascii_uppercase + string.digits, k=len))
+
+    def store_banner(self, bannerblob):
+        with self.connection as conn:
+            with conn.cursor() as curse:
+                # check if table exists
+                sql = """
+                SELECT count(*) FROM banners;
+                """
+                curse.execute(sql)
+                response = list(curse.fetchall())
+                id = self.generate_banner_id()  # unique id
+                if response is None:
+                    # table not exists
+                    sql = """
+                    CREATE TABLE banners (banner_img blob , banner_id varchar(max) );
+                    """
+                    curse.execute(sql)
+
+                    sql = """
+                    INSERT INTO banners VALUE(?,?);
+                    """
+                    curse.execute(sql,(bannerblob,id))
+                    return True
+                else :
+                    sql = """
+                                       INSERT INTO banners VALUE(?,?);
+                                       """
+                    curse.execute(sql, (bannerblob, id))
+                    return True
+
+    def get_banners(self):
+        with self.connection as conn:
+            with conn.cursor() as curse:
+                sql = """
+                               SELECT count(*) FROM banners;
+                               """
+                curse.execute(sql)
+                response = list(curse.fetchall())
+                if response is None:
+                    return response
+                else :
+                    sql = """
+                    SELECT * FROM banners;
+                    """
+                    curse.execute(sql)
+                    response = curse.fetchall()
+                    data = []
+                    for i in response:
+                         data.append({"banner":i[0],"banner_id":i[1]})
+
+                    return data
+
+
