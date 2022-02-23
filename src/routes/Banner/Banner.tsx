26a001b312ac43de961ab25af12a93291113a0b7
geekyUtkarsh99 <utkarshchoudhary80@gmail.com>
2022-02-23 18:55:05 +0530

    frontend server call

--- src/routes/Banner/Banner.tsx
+++ src/routes/Banner/Banner.tsx
@@ -1,34 +1,71 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Button } from "react-bootstrap";
 import './Banner.css'
+import { useState } from "react";
+import { resolve } from "node:path/win32";
+import { read } from "node:fs";
+import axios from "axios";
 
-
-export default class Banner extends React.Component{
-
-constructor(props:any){
-super(props);
-
-
+function dataURItoBlob(dataURI:any) {
+    var byteString = atob(dataURI.split(',')[1]);
+    var ab = new ArrayBuffer(byteString.length);
+    var ia = new Uint8Array(ab);
+    for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+    }
+    var bb = new Blob([ab]);
+    return bb;
 }
 
+const Banner = () =>{
 
+    const fileRef = useRef
 
-render(){
+    const upload = (e:React.ChangeEvent<HTMLInputElement>) =>{
+        let reader = new FileReader()
+        reader.onload = (e)=>{
+            var result = e.target?.result
+           alert(result)
+           var blob = dataURItoBlob(result)
+           console.log(blob)
+           let json = JSON.stringify({
+               'banner':blob
+           })
+           var res =  axios.post('http://64.227.161.183/addbanner',json,{
+               headers:{
+                'Content-Type': 'application/json'
+               }
+           })
+           res.then(response=>{
+               console.log(response.status)
+           })
+        }
+ 
+            e.target.files instanceof FileList
+            ?reader.readAsDataURL(e.target.files[0]): Error('null value detected')
 
+    }
+
+   
 
 return(<div className="parent">
 
 <text className="text">Click on the below "Upload" button to upload banners(images).</text>
 
-<button className="btn">UPLOAD</button>
 
+<label htmlFor="contained-button-file">
+<button className="btn">UPLOAD</button>
+</label>
+<input
+type='file'
+accept="image/*"
+style={{ display: 'none' }}
+id="contained-button-file"
+onChange={(e)=>{upload(e)}}
+/>
 </div>)
 
 }
 
 
-
-
-
-
-}
+export default Banner;
