bb8d65df3a350faeb3805d463b8199a31444bd1e
geekyUtkarsh99 <utkarshchoudhary80@gmail.com>
2022-03-14 17:34:54 +0530

    branch update

--- edugateApi/app.py
+++ edugateApi/app.py
@@ -96,7 +96,7 @@ def get_notes():
 @app.route('/addbranch', methods=['GET'])
 def add_branch():
     args = request.args
-    response = mysql.add_branches(args['branch'], args['sem'],args['year'])
+    response = mysql.add_branches(str(args['branch']).upper(), args['sem'],args['year'])
     if response:
         return make_response(jsonify({'message': 'added'})), 201
     else:
